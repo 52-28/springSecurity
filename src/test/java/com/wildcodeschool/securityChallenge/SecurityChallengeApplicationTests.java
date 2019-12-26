8e7e04a5484379d4e27d7bdbed678c691d09ff4e
52-28 <aymeric.feillault@gmail.com>
2019-12-26 10:43:52 +0100

    initialisation projet

---
+++ src/test/java/com/wildcodeschool/securityChallenge/SecurityChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.securityChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
